eb257c676141649ace46693397a930db19ad1c58
NehaThakur_Mantra <neha.t@mantralabsglobal.com>
2021-10-18 20:00:34 +0530

    sqlite crud operation demo added

---
+++ app/src/main/java/com/example/myapplication_sqlite_demo/model/EmpModelClass.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication_sqlite_demo.model
+
+class EmpModelClass (var userId: Int,
+                     val userName:String ,
+                     val userEmail: String) {
+
+}
